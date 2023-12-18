26506be4121bb9ea8c482664039d674d78f2ee5d
Rick South <rick@natamus.com>
2023-12-18 10:02:52 +0100

    (v3.3) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/shulkerdropstwo/util/Reference.java
+++ Common/src/main/java/com/natamus/shulkerdropstwo/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.shulkerdropstwo.util;
 public class Reference {
 	public static final String MOD_ID = "shulkerdropstwo";
 	public static final String NAME = "Shulker Drops Two";
-	public static final String VERSION = "3.2";
+	public static final String VERSION = "3.3";
 	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
 }
