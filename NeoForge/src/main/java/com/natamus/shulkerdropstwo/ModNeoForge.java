f77496a287b4fcc412e64c196ce6221d363f57cd
Rick South <rick@natamus.com>
2024-07-28 07:15:50 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/shulkerdropstwo/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/shulkerdropstwo/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.shulkerdropstwo;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.shulkerdropstwo.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.shulkerdropstwo.neoforge.events.NeoForgeShulkerEvent;
 import com.natamus.shulkerdropstwo.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
