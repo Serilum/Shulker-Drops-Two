44aee23257583d191624421a0d4e833953d359e5
Rick South <rick@natamus.com>
2025-02-12 23:14:33 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/shulkerdropstwo/forge/events/ForgeShulkerEvent.java
+++ Forge/src/main/java/com/natamus/shulkerdropstwo/forge/events/ForgeShulkerEvent.java
@@ -4,12 +4,10 @@ import com.natamus.shulkerdropstwo.events.ShulkerEvent;
 import net.minecraft.world.entity.Entity;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeShulkerEvent {
 	@SubscribeEvent
-	public void mobItemDrop(LivingDropsEvent e) {
+	public static void mobItemDrop(LivingDropsEvent e) {
 		Entity entity = e.getEntity();
 		ShulkerEvent.mobItemDrop(entity.level(), entity, null);
 	}
