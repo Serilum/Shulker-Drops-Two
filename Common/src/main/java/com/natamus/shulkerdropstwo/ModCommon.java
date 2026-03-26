a6ac8728b9a7d521f6a1dccae87de352c92924a3
Rick South <rick@natamus.com>
2026-03-26 01:29:30 +0100

    Shulker Drops Two source code for 26.1.0.

---
+++ Common/src/main/java/com/natamus/shulkerdropstwo/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.shulkerdropstwo;
+
+import com.natamus.shulkerdropstwo.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
---
+++ NeoForge/src/main/java/com/natamus/shulkerdropstwo/neoforge/events/NeoForgeShulkerEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.shulkerdropstwo.neoforge.events;
+
+import com.natamus.shulkerdropstwo.events.ShulkerEvent;
+import net.minecraft.world.entity.Entity;
+import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+
+public class NeoForgeShulkerEvent {
+	@SubscribeEvent
+	public static void mobItemDrop(LivingDropsEvent e) {
+		Entity entity = e.getEntity();
+		ShulkerEvent.mobItemDrop(entity.level(), entity, null);
+	}
+}
